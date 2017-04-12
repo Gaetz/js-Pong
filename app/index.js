7fd955fe3ca22cc7e1a25738172677d7767d2871
Gaetz <gaetz@rufflerim.com>
2017-04-12 08:35:08 +0200

    Manage scores

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import Ball from './ball';
 import Background from './background';
 import Paddle from './paddle';
+import ScoreManager from './scoreManager';
 import {
     BALL_START_X, BALL_START_Y,
     PADDLE_PLAYER_START_X, PADDLE_PLAYER_START_Y, PADDLE_OPPONENT_START_X, PADDLE_OPPONENT_START_Y,
@@ -9,7 +10,7 @@ import {
 
 let canvas, canvasContext;
 let ball, background, leftPaddle, rightPaddle;
-let playerScore = 0, opponentScore = 0;
+let score;
 
 /**
  * Game start
@@ -39,6 +40,7 @@ function load() {
     ball = new Ball(BALL_START_X, BALL_START_Y);
     leftPaddle = new Paddle(PADDLE_PLAYER_START_X, PADDLE_PLAYER_START_Y);
     rightPaddle = new Paddle(PADDLE_OPPONENT_START_X, PADDLE_OPPONENT_START_Y);
+    score = new ScoreManager();
 }
 
 /**
@@ -56,8 +58,10 @@ function update() {
             ball.speedX *= -1;
         // Ball missed
         else {
-            if (ball.x < 0)
+            if (ball.x < 0) {
+                score.opponentMarks();
                 ball.reset();
+            }
         }
     }
     // Right side
@@ -67,8 +71,10 @@ function update() {
             ball.speedX *= -1;
         // Ball missed
         else {
-            if (ball.x > canvas.width)
+            if (ball.x > canvas.width) {
+                score.playerMarks();
                 ball.reset();
+            }
         }
     }
 }
@@ -103,5 +109,5 @@ function calculateMousePos(evt) {
  * Draw game scores
  */
 function drawScore(canvasContext) {
-    canvasContext.fillText(playerScore + " : " + opponentScore, 380, 20);
+    canvasContext.fillText(score.playerScore + " : " + score.opponentScore, 380, 20);
 }
---
+++ app/scoreManager.js
@@ -0,0 +1,22 @@
+export default class ScoreManager {
+
+    constructor() {
+        this.playerScore = 0;
+        this.opponentScore = 0;
+    }
+
+    /**
+     * Player gains a point
+     */
+    playerMarks() {
+        this.playerScore++;
+    }
+
+    /**
+     * Opponent gains a point
+     */
+    opponentMarks() {
+        this.opponentScore++;
+    }
+
+}
